fc16aeb2699d670afc78633b4c3d8616e8971e83
Nicholas Tierney <nicholas.tierney@gmail.com>
2020-06-26 13:50:47 +1000

    :rocket:

---
+++ demo.R
@@ -0,0 +1,34 @@
+# Demo - picking colours
+colorspace::hcl_color_picker()
+
+# Demo - evaluating colours
+# http://hclwizard.org/
+  
+# Demo - evaluating ggplots
+
+# Assessing colour palettes?
+## Demoplot
+## Specplot
+## Protan/Deutan/Tritan (w demoplot / specplot)
+
+
+# How to assess existing images / plots?
+#   - If it's an image: cvd_emulator
+#   - If it's a ggplot object, `colorblindr::cvd_fig`
+# How to create your own?
+#   - Specify start/end points to travel through:
+#   - qualitative_hcl()
+# - sequential_hcl()
+# - diverge_hcl()
+# - Tweak Hue/Chroma/Luminance?
+#   - lighten/darken
+# - desaturate() (by chroma removal)
+# - max_chroma()
+# - mix_color
+# - Want to pick and point at an app?
+#   - hclwizard
+# - colorspace::choose_color()
+# - colorspace::choose_palette()
+# - colorspace::hcl_wizard()
+
+
